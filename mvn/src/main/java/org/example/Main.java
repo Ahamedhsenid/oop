d308b71e4a7d437401852532ab72f8db105d8b54
Ahamed-hsenid <ahamedhsenid@ahamed_personal_laptop.hsenid.lk>
2024-07-05 14:20:32 +0530

    maven files

--- mvn/src/main/java/org/example/Main.java
+++ mvn/src/main/java/org/example/Main.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("test files");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to square: ");
+        System.out.println("test commit");
+        int number=sc.nextInt();
+        System.out.println("Square of number is: "+ Squaremethod(number));
+
+
+        System.out.println("\nstudent details: ");
+        List<String> Subjects= Arrays.asList("maths","english","history");
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Jane Doe");
+        student.setAge(20);
+        student.setSubjects(Subjects);
+        System.out.println("ID: "+student.getId());
+        System.out.println("name: "+student.getName());
+        System.out.println("age: "+student.getAge());
+        System.out.println("subjects: "+student.getSubjects());
+
+    }
+
+
+    public static int Squaremethod(int number){
+        return number*number;
+
+
     }
 }
