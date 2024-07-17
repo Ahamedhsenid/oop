21f9ae28d83bf7c4b3375ea3e50c1283f22a26c3
Ahamed-hsenid <ahamedhsenid@ahamed_personal_laptop.hsenid.lk>
2024-07-17 08:43:44 +0530

    resolved changes maven

--- mvn/src/main/java/org/example/Student.java
+++ mvn/src/main/java/org/example/Student.java
@@ -13,38 +13,6 @@ public class Student {
 
 
 
-//    public Student (int id,String name,int age, List<String>Subjects){
-//        this.id=age;
-//        this.name=name;
-//        this.age=age;
-//        this.Subjects=Subjects;
-//    }
-//
-//
-//    public int getId(){
-//        return age;
-//
-//    }
-//    public void setId(int id){
-//        this.id=id;
-//    }
-//    public int getAge(){
-//        return age;
-//    }
-//    public void setAge(int age){
-//        this.age=age;
-//    }
-//    public String getName(){
-//        return name;
-//    }
-//    public void setName(String name){
-//        this.name=name;
-//    }
-//    public List<String> getSubjects(){
-//        return Subjects;
-//    }
-//    public void setSubjects(List<String>Subjects){
-//        this.Subjects=Subjects;
-//    }
+
 
 }
